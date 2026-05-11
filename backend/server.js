781401da453fea1ad44d856c9aa481063c815300
sandeeptiwari21 <nathtiwarisandeep@gmail.com>
2026-05-12 02:21:50 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const port = process.env.PORT || 4000
 //middlewares
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  credentials: true
+}))
 connectDB()
 connectCloudinary()
 
