7785cf269fddcf4710782f9e09606d2d2fc2241d
sandeeptiwari21 <nathtiwarisandeep@gmail.com>
2026-05-13 20:22:12 +0530

    fixed frontend origin

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000
 app.use(express.json())
 
 app.use(cors({
-  origin: 'https://doctrapointment-1.onrender.com',
+  origin: 'https://docotrapointment-1.onrender.com',
   credentials: true
 }))
 connectDB()
