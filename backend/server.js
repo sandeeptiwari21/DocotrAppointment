2f6cc7c0806b5ec3a4d17209ad38c8b1eee41db1
sandeeptiwari21 <nathtiwarisandeep@gmail.com>
2026-05-13 20:12:47 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const port = process.env.PORT || 4000
 app.use(express.json())
 
 app.use(cors({
-  origin: '*'
+  origin: 'https://doctrapointment-1.onrender.com',
+  credentials: true
 }))
 connectDB()
 connectCloudinary()
